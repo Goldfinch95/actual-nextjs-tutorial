3d932d91ae93ee83e16afe7bd6fd4eb4be518b6c
Goldfinch <facu_vila@hotmail.com>
2025-03-05 13:58:58 -0300

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+
+const Customers = () => {
+    return (
+        <h1>El contenido del Customers</h1>
+    );
+};
+
+export default Customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+
+const Invoices = () => {
+    return (
+        <h1>El contenido del Invoices</h1>
+    );
+};
+
+export default Invoices;
